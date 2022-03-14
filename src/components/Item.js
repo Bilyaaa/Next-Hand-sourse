b41b1dd97196558007ced10f6104c53419c7d967
Bilyaaa <bilyaa0@gmail.com>
2022-03-14 10:46:30 +0200

    Remaking Shop page

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,40 +1,57 @@
 import { Link } from 'react-router-dom'
 import { Card, Col } from 'react-bootstrap'
 import {DEVICE_ROUTE} from '../utils/consts'
+import '../styles/Item.scss'
 
 
 
-function Item({item}) {
+function ItemLeft({item}) {
     return (
         
-        <Col xl={4} lg={6} key={item.id} style={{ paddingBottom: "2rem" }}>
+       
       <Link to={DEVICE_ROUTE + "/" +  item.id} style={{textDecoration:'none', color:'grey'}}>
-        <Card
-          key={item.id}
-          style={{
-             border: "none",
-             boxShadow: "0 0 10px grey",
-             borderRadius: '0'
-
-            }}
-        >
-          <Card.Img
-           style={{
-            borderRadius: '0'
-           }}
-           variant="top" src={item.imgSrc} />
-          <Card.Body>
-            <Card.Title>
-              <span style={{fontWeight: '300'}}>{item.type}</span> {item.brand}
-            </Card.Title>
-            <Card.Text>{item.price} грн</Card.Text>
-          </Card.Body>
+        <Card className='container__card' key={item.id}>
+          
+          <div className='container__card__div1'>
+            <div>
+              
+            <Card.Img variant="top" src={item.imgSrc}/>
+            </div>
+            <div className='container__card__div1__block'>150 ₴<p/>
+            <span className='container__card__div1__block-span'>{item.type}<span className='span__text'>{item.brand}</span></span>
+            </div>
+          </div>
         </Card>
       </Link>
-    </Col>
+    
     )
 }
 
 
 
-export default Item
+export { ItemLeft }
+
+
+function ItemRight({item}) {
+  return (
+      
+      
+    <Link to={DEVICE_ROUTE + "/" +  item.id} style={{textDecoration:'none', color:'grey'}}>
+      <Card className='container__cardR' key={item.id}>
+        
+        <div className='container__card__div1R'>
+          <div>
+            
+          <Card.Img variant="top" src={item.imgSrc}/>
+          </div>
+          <div className='container__card__div1__blockR'>150 ₴<p/>
+          <span className='container__card__div1__block-spanR'>{item.type}<span className='span__textR'>{item.brand}</span></span>
+          </div>
+        </div>
+      </Card>
+    </Link>
+ 
+  )
+}
+
+export { ItemRight }
